812651004e7aafbfc89062242653ae2d8522042e
Joel <jcbluemel@yahoo.com>
2022-07-11 17:06:04 -0700

    added styling to pokecard and pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,19 +1,19 @@
-const basePokeImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
+import './Pokecard.css';
+const BASE_IMG_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
 
 /** Component Pokecard...
- * Takes an Object of a pokemon:
- * {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62}
- * Returns a single Pokemon, with their name, image, and type.
- * <div> <h1>{name}</h1> <img src = {const${id}}/> <p>{type}</p> <p>{base_experience}</p></div>
+ *  Takes an Object of a pokemon:
+ *    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62} =>
  *
+ *  Returns a single Pokemon, with their name, image, type, exp.
  */
 function Pokecard({ id, name, type, base_experience }) {
   return (
-    <span>
-      <h1>{name}</h1>
-      <img src={basePokeImg + `${id}.png`} alt={`This is a ${name}`} />
-      <p>{type}</p>
-      <p>{base_experience}</p>
+    <span className="Pokecard">
+      <h2>{name}</h2>
+      <img src={`${BASE_IMG_URL}${id}.png`} alt={`${name}`}/>
+      <p>Type: {type}</p>
+      <p>EXP: {base_experience}</p>
     </span>
   );
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,22 +1,24 @@
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 import './App.css';
-
-
 
 /** Function Pokedex is a module that creates:
  * Takes an array of pokemon objects.
  * Returns and a sequence of Pokecard components for each pokemon obj in array.
  */
-function Pokedex({pokemon}){
+
+function Pokedex({allPokemon}){
   return (
-    <div className="App">
-      {pokemon.map(mon => <Pokecard id={mon.id} name={mon.name} type={mon.type} base_experience={mon.base_experience}/>)}
+    <div className="Pokedex">
+      {allPokemon.map(mon =>
+        <Pokecard
+          id={mon.id}
+          name={mon.name}
+          type={mon.type}
+          base_experience={mon.base_experience}
+        />)}
     </div>
   )
-
-
-
-
 }
 
 export default Pokedex
